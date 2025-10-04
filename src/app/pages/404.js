204d8608ca36f070b9f3960cb9b9dc3816e8c9ad
Rashmi_B_R <harsharajs841@gmail.com>
2025-10-04 15:11:46 +0530

    v_0.1.8.1

--- src/app/pages/404.js
+++ src/app/pages/404.js
@@ -1,7 +1,25 @@
-export default function NotFound() {
+// pages/404.js
+
+import React from 'react';
+import Head from 'next/head'; // This is safe, as it only injects content into the head.
+
+function Custom404() {
   return (
-    <div>
-      <h1>404 Not Found</h1>
-    </div>
+    <>
+      {/* Head component is safe for meta tags/title */}
+      <Head>
+        <title>404 - Page Not Found</title>
+      </Head>
+      
+      {/* Use standard HTML elements (div, h1, p) inside the body */}
+      <div style={{ textAlign: 'center', paddingTop: '50px' }}>
+        <h1>404</h1>
+        <p>The page you are looking for does not exist.</p>
+        <p>Error Code: No-Document-Import-In-Page</p> 
+        {/* You can add a link back to the homepage here */}
+      </div>
+    </>
   );
 }
+
+export default Custom404;
