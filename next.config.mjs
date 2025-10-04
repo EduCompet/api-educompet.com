e52b45ce1a1d4c7bcc029ec96c467e2d3c1fc6f0
Rashmi_B_R <harsharajs841@gmail.com>
2025-10-04 08:15:47 +0530

    v_0.1.4

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,9 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // use this instead of experimental.serverComponentsExternalPackages
-  serverExternalPackages: ["html-pdf-node", "mammoth"],
-
-  // add other top-level settings if you have them (like images, reactStrictMode, etc.)
   reactStrictMode: true,
+  serverExternalPackages: ["html-pdf-node", "mammoth"],
+  // do NOT use 'output: "export"'
 };
 
 module.exports = nextConfig;
