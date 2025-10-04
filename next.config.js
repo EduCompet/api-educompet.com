7a546bfc7f9efb9a9be0809d85dad12152cff11a
Rashmi_B_R <harsharajs841@gmail.com>
2025-10-04 08:33:30 +0530

    v_0.1.5

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   serverExternalPackages: ["html-pdf-node", "mammoth"],
-  // do NOT use 'output: "export"'
 };
 
 module.exports = nextConfig;
