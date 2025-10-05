d3286fbdc85c354c8dedeb2e0133a232bf9108ed
Rashmi_B_R <harsharajs841@gmail.com>
2025-10-05 12:31:45 +0530

    v_0.2.1

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,7 +1,11 @@
+// src/app/not-found.js
 export default function NotFound() {
   return (
     <div className="flex h-screen items-center justify-center">
-      <h1 className="text-2xl font-bold">404 | Page Not Found</h1>
+      <div>
+        <h2 className="text-xl font-semibold">404 - Page Not Found</h2>
+        <p>The page you are looking for does not exist.</p>
+      </div>
     </div>
   );
 }
